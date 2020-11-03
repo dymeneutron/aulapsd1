20f4402d783d49fdee6d7719ad6822e1367cbc81
dymeneutron <diegoluciano.carvalho@gmail.com>
2020-11-03 19:39:12 -0300

    project creat

---
+++ src/test/java/com/iftm/coursepds1/Coursepds1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.coursepds1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursepds1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
