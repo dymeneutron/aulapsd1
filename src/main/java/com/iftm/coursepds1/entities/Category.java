a50265bf6f07e6323d23baac8e731d1ec884890b
dymeneutron <diegoluciano.carvalho@gmail.com>
2021-01-13 12:37:17 -0300

    Auditing data: PreUpdate & PrePersist

--- src/main/java/com/iftm/coursepds1/entities/Category.java
+++ src/main/java/com/iftm/coursepds1/entities/Category.java
@@ -1,6 +1,7 @@
  package com.iftm.coursepds1.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 
@@ -25,6 +28,8 @@ public class Category implements Serializable {
 	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 
+	private Instant createdAt;
+	private Instant updatedAt;
 	
 	public Category() {	
 	}
@@ -55,6 +60,26 @@ public class Category implements Serializable {
 		return products;
 	}
 	
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		Instant now = Instant.now();
+		updatedAt = now;
+		createdAt = now;
+	}
 
 	@Override
 	public int hashCode() {
