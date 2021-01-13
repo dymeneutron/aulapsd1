7f48edcaf886070a98b12e5d814ffd4d198c323f
dymeneutron <diegoluciano.carvalho@gmail.com>
2021-01-13 12:29:59 -0300

    Pagination

--- src/main/java/com/iftm/coursepds1/services/ProductService.java
+++ src/main/java/com/iftm/coursepds1/services/ProductService.java
@@ -2,13 +2,14 @@ package com.iftm.coursepds1.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,9 +32,9 @@ public class ProductService {     	// classe ira buscar usarios por id e busca p
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	public List<ProductDTO> findAll() {
-		List<Product> list =  repository.findAll();
-		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
+	public Page<ProductDTO> findAllPaged(Pageable pageable) {
+		Page<Product> list =  repository.findAll(pageable);
+		return list.map(e -> new ProductDTO(e));
 	}
 	public ProductDTO  findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
